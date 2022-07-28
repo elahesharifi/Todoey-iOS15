7d125848ce649cdb2a35330d2ba001b052f39bb1
elahe <el.sh192014@gmail.com>
2022-07-28 11:48:40 +0200

    delete comment code

--- Todoey-iOS15/Controllers/TodoListViewController.swift
+++ Todoey-iOS15/Controllers/TodoListViewController.swift
@@ -11,21 +11,12 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     
-    
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-//        let newItem = Item()
-//        newItem.title = "Find Mike"
-//        itemArray.append(newItem)
-//
-//        let newItem2 = Item()
-//        newItem2.title = "Do dishes"
-//        itemArray.append(newItem2)
-//
+
         loadItems()
     }
     //MARK: - Tableview Datasource Methods
