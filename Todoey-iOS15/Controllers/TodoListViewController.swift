e8f6167450520dda0fe19e1916e6f563c245aa7b
elahe <el.sh192014@gmail.com>
2022-08-02 11:47:35 +0200

    add searchbar

--- Todoey-iOS15/Controllers/TodoListViewController.swift
+++ Todoey-iOS15/Controllers/TodoListViewController.swift
@@ -8,18 +8,17 @@
 import UIKit
 import CoreData
 
-class TodoListViewController: UITableViewController {
+class TodoListViewController: UITableViewController, UISearchBarDelegate {
     
     var itemArray = [Item]()
     
-  
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
         loadItems()
     }
     //MARK: - Tableview Datasource Methods
