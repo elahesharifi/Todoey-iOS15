c125ae1de222b31e3308e5ec2a0d30d441e1449f
elahe <el.sh192014@gmail.com>
2022-08-04 00:19:49 +0200

    test Create om CRUD

--- Todoey-iOS15/AppDelegate.swift
+++ Todoey-iOS15/AppDelegate.swift
@@ -7,13 +7,20 @@
 
 import UIKit
 import CoreData
-
+import RealmSwift
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        do {
+        let realm = try Realm()
+        } catch {
+            print("Error intialising new realm,\(error)")
+        }
+        
         return true
     }
     //MARK: - UISceneSession Lifecycle
