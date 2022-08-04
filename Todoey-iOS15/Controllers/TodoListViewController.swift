418ca6b7fef98207dcb3f84d91e306b9b7ee27cc
elahe <el.sh192014@gmail.com>
2022-08-04 11:51:38 +0200

    Remove Data from Realm

--- Todoey-iOS15/Controllers/TodoListViewController.swift
+++ Todoey-iOS15/Controllers/TodoListViewController.swift
@@ -54,20 +54,14 @@ class TodoListViewController: UITableViewController{
         if let item = todoItems?[indexPath.row] {
             do {
                 try realm.write{
-                    item.done = !item.done
+                    realm.delete(item)
+                // item.done = !item.done
                 }
             } catch {
                 print("Error saving done status,\(error)")
             }
-            
         }
         tableView.reloadData()
-        //        context.delete(itemArray[indexPath.row])
-        //        itemArray.remove(at: indexPath.row)
-        
-        //  todoItems[indexPath.row].done = !todoItems[indexPath.row].done
-        
-        //  saveItems()
     
         tableView.deselectRow(at: indexPath, animated: false)
     }
