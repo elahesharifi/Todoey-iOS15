e6e371b6a6ebb933d55abbff207e7740c8e11e95
elahe <el.sh192014@gmail.com>
2022-08-09 09:41:32 +0200

    delete some code from CoreData

--- Todoey-iOS15/AppDelegate.swift
+++ Todoey-iOS15/AppDelegate.swift
@@ -6,17 +6,16 @@
 //
 
 import UIKit
-import CoreData
 import RealmSwift
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        print(Realm.Configuration.defaultConfiguration.fileURL)
+        //print(Realm.Configuration.defaultConfiguration.fileURL)
         
         do {
-        let realm = try Realm()
+         _ = try Realm()
         } catch {
             print("Error intialising new realm,\(error)")
         }
@@ -31,30 +30,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-    }
-    // MARK: - Core Data stack
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "Todoey_iOS15")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-    
-    // MARK: - Core Data Saving support
-    
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
     }
     
 }
--- Todoey-iOS15/Data Model/Item.swift
+++ Todoey-iOS15/Data Model/Item.swift
@@ -12,6 +12,6 @@ class Item: Object{
     // when you want to add variable in RealmSwift you should use @objc dynamic
     @objc dynamic var title: String = ""
     @objc dynamic var done: Bool = false
-    
+    @objc dynamic var dateCreated: Date?
     var parentCategory = LinkingObjects(fromType: Category.self, property:"items")
 }
